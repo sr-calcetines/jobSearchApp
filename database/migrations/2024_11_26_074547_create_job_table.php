395bb292ab42dd5ec282ac475856c92a100c894d
sr-calcetines <pgavilansanc@gmail.com>
2024-11-26 09:13:24 +0100

    .env database conection and job table schema

---
+++ database/migrations/2024_11_26_074547_create_job_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job', function (Blueprint $table) {
+            $table->id();
+            $table->string("enterprise");
+            $table->string("position");
+            $table->timestamps();
+            $table->boolean("state");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job');
+    }
+};
