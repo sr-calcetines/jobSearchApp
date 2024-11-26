4ab37c327f5ac53b79c272f470852beacebf160c
sr-calcetines <pgavilansanc@gmail.com>
2024-11-26 15:30:54 +0100

    style: added header, footer and contetnt for the page; also added styles for each one

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+    <div class="container">
+        <h1 class="app-name">Job Search App</h1>
+        <p>José Ignacio Gavilán Sánchez &copy; 2024</p>
+
+        <div class="social-links">
+
+            <a href="https://www.linkedin.com/in/pepe-gavilán-sánchez-6b591a261" target="_blank" class="social-link">
+                <img src="https://cdn-icons-png.flaticon.com/256/174/174857.png" alt="LinkedIn Profile"
+                    class="social-icon">
+            </a>
+            
+            <a href="https://github.com/sr-calcetines" target="_blank" class="social-link">
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub Profile" class="social-icon">
+            </a>
+
+            <a href="mailto:pgavilansanc@gmail.com" target="_blank" class="social-link">
+                <img src="https://cdn-icons-png.flaticon.com/512/281/281769.png" alt="GitHub Profile"
+                    class="social-icon">
+            </a>
+
+        </div>
+    </div>
+</footer>
