a41850f1ecf5b7a640314807090f3a2eddbb4846
sr-calcetines <pgavilansanc@gmail.com>
2024-11-27 11:48:11 +0100

    test: added api test archive and first api test

---
+++ tests/Feature/Api/OfferTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use App\Models\Offer;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class OfferTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_CheckIfRecieveAllEntryOfOfferInJsonFile(){
+        $offer = Offer::factory(2)->create();
+
+        $response = $this->get(route('apihome'));
+       
+        $response->assertStatus(200)
+                 ->assertJsonCount(2);
+
+    }
+}
