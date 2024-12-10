0c8a37747142d7698801fdc503ffd58dd6313bdb
sr-calcetines <pgavilansanc@gmail.com>
2024-12-10 10:31:43 +0100

    feat: added crud for follows table in api (controller methods, routes and test doned)

--- app/Http/Controllers/Api/FollowController.php
+++ app/Http/Controllers/Api/FollowController.php
@@ -10,6 +10,13 @@ use App\Http\Controllers\Controller;
 class FollowController extends Controller
 {
     /**
+     * Display a listing of the resource.
+     */
+    public function index(string $id)
+    {
+        return response()->json(Offer::find($id)->follows, 200);
+    }
+    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request, $offerId)
@@ -34,5 +41,35 @@ class FollowController extends Controller
         $offer->follows()->createMany($followsData);
 
         return response()->json(['message' => 'News added correctly', 'offer' => $offer->load('follows')]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $offerId, string $newsId)
+    {
+        return response()->json(Offer::find($offerId)->follows[(int)$newsId - 1],200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $offerId, string $newsId)
+    {
+        $follows = Offer::find($offerId)->follows[(int)$newsId - 1];
+
+        $follows->update([
+            'offers_id' => $request-> offers_id,
+            'news' => $request->news,
+        ]);
+        return response()->json($follows, 200); 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $offerId, string $newsId)
+    {
+        Offer::find($offerId)->follows[(int)$newsId - 1]->delete();
     }
 }
